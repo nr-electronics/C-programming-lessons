4e3a34964991cc49aa5ba8e87bb585d22ee42570
NR.electronics <48318430+nr-electronics@users.noreply.github.com>
2019-05-19 18:47:26 +0300

    Create hello world.c
    Hello world!

---
+++ lesson 3/hello world.c
@@ -0,0 +1,12 @@
+//Версия 1.0
+//Программа вывода "Hello,  world -Здравствуй мир" на дисплей
+#include <stdio.h>
+
+//начало функции main													
+	int main(void)
+	    {
+      
+	    printf ( "Hello,  world - Здравствуй мир\n");
+	    return 0;
+      
+	    }//окончание функции main
